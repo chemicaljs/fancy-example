7e3467ff93cb0580845bf163a1425b1fd73202b9
Nebelung <nebelung@mailfence.com>
2024-08-31 06:31:08 +0000

    Upload fancy example

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# fancy-example
+# Fancy Example
 An advanced example using Chemical components.
+
+## Setup
+First, clone the GitHub repo.
+
+```bash
+git clone https://github.com/chemicaljs/fancy-example.git
+```
+
+Next, run `npm install` to install dependencies.
+
+Finally run `npm start` to start the app on `localhost:3000`.
+
+## License
+Fancy Example uses the AGPL 3.0 license.
---
+++ index.js
@@ -0,0 +1,16 @@
+import { ChemicalServer } from "chemicaljs";
+import express from "express";
+
+const [app, listen] = new ChemicalServer();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public", {
+    index: "index.html",
+    extensions: ["html"]
+}));
+
+app.serveChemical();
+
+listen(port, () => {
+    console.log(`Fancy example listening on port ${port}`);
+});
